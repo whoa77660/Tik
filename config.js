01d7e1e948a34c2146c68f5b3d5047100f38624a
whoa77660 <whoa77660@gmail.com>
2026-08-18 18:50:37 +0600

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ module.exports = {
   rapidApi: {
     host: 'tiktok-scraper7.p.rapidapi.com',
     keys: [
-      // 'YOUR_RAPIDAPI_KEY_1',
-      // 'YOUR_RAPIDAPI_KEY_2',
-      // 'YOUR_RAPIDAPI_KEY_3'
+       '88451e46a1msh4da5011a9b08fefp1cd049jsn0fb24108803a',
+       '25d664956fmsh8528a6b12b10b8ap11d93cjsn23c8df392588',
+       '5ed7dcbd27msh819c421722d2f09p1da46ajsne996e42b9e06'
     ],
     cacheTtl: 300
   }
